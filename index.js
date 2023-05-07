10eaa40a815313179062a384ac435142cec54d76
Veronica <vveronikanika@gmail.com>
2023-05-07 21:19:55 +0300

    feat: add error handlers when it is impossible to read argv files with paths

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-// const path = require('path');
-
-
-
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -19,13 +16,27 @@ rl.question('Enter path to HTML template: ', (answer) => {
   rl.question('Enter path to data file: ', (answer) => {
     dataPath = answer;
 
-    const html = fs.readFileSync(htmlPath, 'utf-8');
-    const data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+		let html;
+		try {
+			html = fs.readFileSync(htmlPath, 'utf-8');
+		} catch (error) {
+			console.error(`Error reading HTML file: ${error.message}`);
+      rl.close();
+      return;
+		}
+
+    let data;
+		
+		try {
+			data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+		} catch (error) {
+			console.error(`Error parsing data file: ${error.message}`);
+      rl.close();
+      return;
+		}
 
     const server = http.createServer((req, res) => {
       const parsedUrl = url.parse(req.url, true);
-      // const filePath = path.join(__dirname, parsedUrl.pathname);
-
 
       if (parsedUrl.pathname === '/') {
         res.setHeader('Content-Type', 'text/html');
